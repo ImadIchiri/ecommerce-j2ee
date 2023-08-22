ac3ee4a8f5e4e6b0e2033b590b11762945f89503
ImadIchiri <88448157+ImadIchiri@users.noreply.github.com>
2023-08-22 15:04:52 +0100

    modifier Panier et LignePanier

--- src/main/java/com/ecommerce/Entity/Panier.java
+++ src/main/java/com/ecommerce/Entity/Panier.java
@@ -1,5 +1,28 @@
 package com.ecommerce.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Panier {
+	private static int idCounter = 0;
 	private int id;
+	private List<LignePanier> listLignePanier = new ArrayList<LignePanier>();
+
+	public Panier() {
+		this.id = idCounter;
+		idCounter++;
+	}
+	
+	public Panier(int id) {
+		this.id = idCounter;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
 }
